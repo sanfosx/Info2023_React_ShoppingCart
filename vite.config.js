7b2629c33cb97837d41b126dd713960710a38b2a
Santiago S. Foschiatti <sanfosx@gmail.com>
2023-06-07 15:28:05 -0300

    corrijiendo la config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sanfosx.github.io/vite-deploy'
+  base: 'https://sanfosx.github.io/Info2023_React_ShoppingCart'
 })
